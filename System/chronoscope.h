2a00bcd1208648822a968bb214a94555fd1531f2
雨山 <1498127009@qq.com>
2024-02-06 11:24:34 +0800

    modified:   System/chronoscope.h

--- System/chronoscope.h
+++ System/chronoscope.h
@@ -4,6 +4,6 @@ void TIM1_Int_Init(void);//定时器1
 void Clock1_Start();
 u16 Clock1_End();
 void TIM4_Int_Init(void);
-void Clock2_Start();//定时器4，无用
+void Clock2_Start();//定时器4，打不开Tim4，无用
 u16 Clock2_End();
 #endif
