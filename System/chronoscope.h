aded433cb97f272daded627c300486d54c783d9a
雨山 <1498127009@qq.com>
2024-02-06 11:38:59 +0800

    修改注释

--- System/chronoscope.h
+++ System/chronoscope.h
@@ -4,6 +4,6 @@ void TIM1_Int_Init(void);//定时器1
 void Clock1_Start();
 u16 Clock1_End();
 void TIM4_Int_Init(void);
-void Clock2_Start();//定时器4
+void Clock2_Start();//定时器4，目前无用
 u16 Clock2_End();
 #endif
