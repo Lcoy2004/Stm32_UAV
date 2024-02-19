d3e4c9e4075c4a917811619fdca031aa551cfdb4
雨山 <1498127009@qq.com>
2024-02-19 18:24:22 +0800

    新添Receive文件

---
+++ User/Receive.c
@@ -0,0 +1,31 @@
+#include "stm32f10x.h" 
+#include "ESP.h"
+
+
+int num;
+		float sum;
+
+uint8_t ReceiveVis(void)
+{
+	if(ESP_GetRxFlag()==1)
+	{
+		return Vis;//0：急停 1：左 2：后 3：前 4：右 5：上升高度
+		//num=HexNum;
+		//sum=(float)num;
+		//OLED_ShowChar(1,1, Vis);
+		//OLED_ShowHexNum(3,1,sum,3);
+	}
+}
+
+float ReceiveNum(void)
+{
+	if(ESP_GetRxFlag()==1)
+	{
+		
+		num=HexNum;
+		sum=(float)num;
+		//OLED_ShowChar(1,1, Vis);
+		//OLED_ShowHexNum(3,1,sum,3);
+		return sum;
+	}
+}
---
+++ User/Receive.h
@@ -0,0 +1,10 @@
+#ifndef __RECEIVE_H
+#define __RECEIVE_H
+
+extern int num;
+extern float sum;
+
+uint8_t ReceiveVis(void);
+float ReceiveNum(void);
+
+#endif
