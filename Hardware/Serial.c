4580f35be3a0d25e25f38150e7bcb59422bc2ef2
郭峻廷 <14054470+YvDong@user.noreply.gitee.com>
2024-02-27 13:09:28 +0000

    update Hardware/Serial.c. 去除冗余代码
    Signed-off-by: 郭峻廷 <14054470+YvDong@user.noreply.gitee.com>

--- Hardware/Serial.c
+++ Hardware/Serial.c
@@ -44,54 +44,7 @@ void Serial_Init(void)
 	USART_Cmd(USART1, ENABLE);
 }
 
-void Serial_SendByte(uint8_t Byte)
-{
-	USART_SendData(USART1, Byte);
-	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-}
 
-void Serial_SendArray(uint8_t *Array, uint16_t Length)
-{
-	uint16_t i;
-	for (i = 0; i < Length; i ++)
-	{
-		Serial_SendByte(Array[i]);
-	}
-}
-
-void Serial_SendString(char *String)
-{
-	uint8_t i;
-	for (i = 0; String[i] != '\0'; i ++)
-	{
-		Serial_SendByte(String[i]);
-	}
-}
-
-uint32_t Serial_Pow(uint32_t X, uint32_t Y)
-{
-	uint32_t Result = 1;
-	while (Y --)
-	{
-		Result *= X;
-	}
-	return Result;
-}
-
-void Serial_SendNumber(uint32_t Number, uint8_t Length)
-{
-	uint8_t i;
-	for (i = 0; i < Length; i ++)
-	{
-		Serial_SendByte(Number / Serial_Pow(10, Length - i - 1) % 10 + '0');
-	}
-}
-
-int fputc(int ch, FILE *f)
-{
-	Serial_SendByte(ch);
-	return ch;
-}
 
 void Serial_Printf(char *format, ...)
 {
